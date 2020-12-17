21af2646bf344fba1a123866ab0e1875df372745
Spyros <spyros.f4@gmail.com>
2020-12-18 00:46:40 +0200

    replace " " with ' '

--- address.js
+++ address.js
@@ -1,17 +1,17 @@
-const Router = require("koa-router");
+const Router = require('koa-router');
 
 const router = new Router();
 
-const Web3 = require("web3");
+const Web3 = require('web3');
 
 const web3 = new Web3(
   new Web3.providers.HttpProvider(
-    "https://kovan.infura.io/v3/3269c013c5b2449aaea1bb593f873d77"
+    'https://kovan.infura.io/v3/3269c013c5b2449aaea1bb593f873d77'
   )
 );
 
-const contractAddress = "0xf019a9c291fab47003588fe74d33660cd467fc22";
-const abi = require("./abi.js");
+const contractAddress = '0xf019a9c291fab47003588fe74d33660cd467fc22';
+const abi = require('./abi.js');
 const contract = new web3.eth.Contract(abi, contractAddress);
 
 
@@ -23,16 +23,16 @@ class ResponseData {
   }
 }
 
-router.get("/addresses/:ethAddress/transactions", async (ctx, next) => {
+router.get('/addresses/:ethAddress/transactions', async (ctx, next) => {
   const addressParam = Object.values(ctx.params).toString(); // address we get from url for task4
   const ResponseArr = [];
   console.log(addressParam);
   if (web3.utils.toChecksumAddress(addressParam)) {
     await contract.getPastEvents(
-      "Transfer",
+      'Transfer',
       {
         fromBlock: 0,
-        toBlock: "latest",
+        toBlock: 'latest',
       },
       (err, events) => {
         const transfers = events.map((e) => e.returnValues);
@@ -49,7 +49,7 @@ router.get("/addresses/:ethAddress/transactions", async (ctx, next) => {
     });
   } else {
     ctx.response.status = 404;
-    ctx.body = "Wrong Eth Address";
+    ctx.body = 'Wrong Eth Address';
   }
   next();
 });
--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-const Web3 = require("web3");
+const Web3 = require('web3');
 const web3 = new Web3(
   new Web3.providers.HttpProvider(
-    "https://kovan.infura.io/v3/3269c013c5b2449aaea1bb593f873d77"
+    'https://kovan.infura.io/v3/3269c013c5b2449aaea1bb593f873d77'
   )
 );
 
-const contractAddress = "0xf019a9c291fab47003588fe74d33660cd467fc22";
+const contractAddress = '0xf019a9c291fab47003588fe74d33660cd467fc22';
 
-const abi = require("./abi.js");
+const abi = require('./abi.js');
 
 const contract = new web3.eth.Contract(abi, contractAddress);
 
 contract.methods.name().call((err, result) => {
-  console.log("name:", result);
+  console.log('name:', result);
 });
 
 contract.methods.symbol().call((err, result) => {
-  console.log("symbol:", result);
+  console.log('symbol:', result);
 });
 
 contract.methods.decimals().call((err, result) => {
-  console.log("decimals:", result);
+  console.log('decimals:', result);
 });
 
 contract.getPastEvents(
-  "Transfer",
+  'Transfer',
   {
     fromBlock: 0,
-    toBlock: "latest",
+    toBlock: 'latest',
   },
   (err, events) => {
     const transfers = events.map((e) => e.returnValues);
     transfers.forEach((transfer) => {
-      console.log("from:", transfer.from);
-      console.log("to:", transfer.to);
-      console.log("value:", transfer.value);
+      console.log('from:', transfer.from);
+      console.log('to:', transfer.to);
+      console.log('value:', transfer.value);
     });
   }
 );
