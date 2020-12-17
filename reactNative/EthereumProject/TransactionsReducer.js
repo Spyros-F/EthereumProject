44720f2063c8b10146dd8c76b9aa51100d79673e
Spyros <spyros.f4@gmail.com>
2020-12-18 01:55:41 +0200

    remove comments

--- reactNative/EthereumProject/TransactionsReducer.js
+++ reactNative/EthereumProject/TransactionsReducer.js
@@ -1,4 +1,4 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
  
 const INITIAL_STATE = {
   current: [],
@@ -11,20 +11,14 @@ const transactionsReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
       case 'SELECT_TRANSACTION':
         
-        // copy the state 
         const { current,  all_transactions,} = state;
-   
-        //remove a subject from the all_transactions array
-         
+      
         const addedTransactions = all_transactions.splice(action.payload, 1);
    
-        // put subject in current array
         current.push(addedTransactions);
    
-        // update the redux state to reflect the change
         const newState = { current, all_transactions };
          
-        //return new state
         return newState;
    
       default:
