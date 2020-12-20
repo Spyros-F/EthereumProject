0f9b163e7449f06011ed67072fc1e907cafa303a
Spyros <spyros.f4@gmail.com>
2020-12-20 03:08:16 +0200

    converted from class based components into functional components

--- reactNative/EthereumProject/components/Home/Home.js
+++ reactNative/EthereumProject/components/Home/Home.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {connect} from 'react-redux';
-import {styles} from './style'
+import {styles} from './style';
+import {useNavigation} from '@react-navigation/native'
 import {
   View,
   Text,
@@ -8,42 +9,81 @@ import {
   TextInput,
 } from 'react-native';
 
-class Home extends Component {
-  state = {
-    address: '',
-    transactionDataList : []
+const Home = () => {
+
+  const [address, setAddress] = useState('');
+
+  const [transactionDataList] = useState([]);
+
+  const navigation = useNavigation();
+
+  const handleAddress = (text) => {
+    setAddress(text);
   };
-  handleAddress = (text) => {
-    this.setState({address: text});
-  };
-  search = (address) => {
-    this.props.navigation.navigate('Transactions', {
-      address: this.state.address, 
-      transactionDataList : this.state.transactionDataList,
+
+  const search = (address) => {
+    navigation.navigate('Transactions', {
+      address: address, 
+      transactionDataList : transactionDataList,
       transactionDataListLength: -1},
       );
   };
-  render() {
-    return (
-      <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          underlineColorAndroid="transparent"
-          placeholder="EthereumAddress"
-          placeholderTextColor="#9a73ef"
-          autoCapitalize="none"
-          onChangeText={this.handleAddress}
-        />
 
-        <TouchableOpacity
-          style={styles.searchButton}
-          onPress={() => this.search(this.state.address)}>
-          <Text style={styles.searchButtonText}> Search </Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        underlineColorAndroid="transparent"
+        placeholder="EthereumAddress"
+        placeholderTextColor="#9a73ef"
+        autoCapitalize="none"
+        onChangeText={handleAddress}
+      />
+      <TouchableOpacity
+        style={styles.searchButton}
+        onPress={() => search(address)}>
+        <Text style={styles.searchButtonText}> Search </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+// class Home extends Component {
+//   state = {
+//     address: '',
+//     transactionDataList : []
+//   };
+//   handleAddress = (text) => {
+//     this.setState({address: text});
+//   };
+//   search = (address) => {
+//     this.props.navigation.navigate('Transactions', {
+//       address: this.state.address, 
+//       transactionDataList : this.state.transactionDataList,
+//       transactionDataListLength: -1},
+//       );
+//   };
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <TextInput
+//           style={styles.input}
+//           underlineColorAndroid="transparent"
+//           placeholder="EthereumAddress"
+//           placeholderTextColor="#9a73ef"
+//           autoCapitalize="none"
+//           onChangeText={this.handleAddress}
+//         />
+
+//         <TouchableOpacity
+//           style={styles.searchButton}
+//           onPress={() => this.search(this.state.address)}>
+//           <Text style={styles.searchButtonText}> Search </Text>
+//         </TouchableOpacity>
+//       </View>
+//     );
+//   }
+// }
 
 const mapStateToProps = (state) => {
   const { transactions } = state
--- reactNative/EthereumProject/components/Transactions/Transactions.js
+++ reactNative/EthereumProject/components/Transactions/Transactions.js
@@ -1,31 +1,27 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text} from 'react-native';
 import {connect} from 'react-redux';
 import {Table, Row, Rows} from 'react-native-table-component'
 import {API_URL} from '@env'
 import {styles} from './style'
 
-class Transactions extends Component {
+const Transactions = (props) => {
 
-  constructor(props) {
-      super(props);
-      const transactionDataList = this.props.route.params.transactionDataList;
-      const address = this.props.route.params.address;
-      const transactionDataListLength = this.props.route.params.transactionDataListLength;
-      this.state = { 
-         address: address,
-         transactionDataList: transactionDataList,
-         transactionDataListLength: transactionDataListLength
-      }
-   }
-  
-  
-  componentDidMount() {
-    // 'http://192.168.1.4:8080/addresses/0xF7b547f3E46EFfB3480EEE2c486AE760734B135c/transactions'
+  const [address] = useState(props.route.params.address);
+
+  const [transactionDataList, setTransactionDataList] = useState(props.route.params.transactionDataList);
+
+  const [transactionDataListLength, setTransactionDataListLength] = useState(props.route.params.transactionDataListLength);
+
+  const [tableData] = useState([]);
+
+  const [tableHead] = useState(['From', 'To', 'Value']);
+
+  useEffect(() => {
+    //'http://192.168.1.4:8080/addresses/0xF7b547f3E46EFfB3480EEE2c486AE760734B135c/transactions'
     //you need to change the ip to be the on that shows your localhost
     // in cmd you type ipconfig and you get IPv4 Address.
-    const url = `${API_URL}/addresses/${this.props.route.params.address}/transactions`
-  
+    const url = `${API_URL}/addresses/${address}/transactions`
     fetch(url
       ,
       {
@@ -38,47 +34,117 @@ class Transactions extends Component {
     )
     .then((response) => response.json())
     .then((responseJson) => {
-      this.setState({ transactionDataList: responseJson });
-      this.setState( {transactionDataListLength : responseJson.length})
+      setTransactionDataList(responseJson);
+      setTransactionDataListLength(responseJson.length)
     })
     .catch((error) => {
       console.log(error);
-      this.setState({transactionDataListLength : 0})
+      setTransactionDataListLength(0)
     });
-  }
-  
 
-  render() {
-    const tableData = [];
-
-    const tableHead = ['From', 'To', 'Value'];
-
-    const temp = this.state.transactionDataList.flatMap((e) => [e.from,e.to,e.value]);
+    const temp = transactionDataList.flatMap((e) => [e.from,e.to,e.value]);
 
     while(temp.length) {
       tableData.push(temp.splice(0,3));
     }
-    
-    if (this.state.transactionDataListLength > 0) {
+
+  },[transactionDataListLength]);
+
+
+  if (transactionDataListLength > 0) {
+    return (
+      <View>
+        <Table>
+          <Row data={tableHead} style={styles.head}  />
+          <Rows data={tableData} style={styles.row}  />
+        </Table>
+      </View>
+    );
+  } else if (transactionDataListLength === 0) {
       return (
         <View>
-          <Table>
-            <Row data={tableHead} style={styles.head}  />
-            <Rows data={tableData} style={styles.row}  />
-          </Table>
-        </View>
-      );
-     } else if (this.state.transactionDataListLength === 0) {
-       return (
-         <View>
           <Text>The Ethereum address that you searched has no transaction!</Text>
         </View>
       )
-    } else {
-      return null;
-    }   
+  } else {
+    return null;
   }
-}
+} 
+
+// class Transactions extends Component {
+
+//   constructor(props) {
+//       super(props);
+//       const transactionDataList = this.props.route.params.transactionDataList;
+//       const address = this.props.route.params.address;
+//       const transactionDataListLength = this.props.route.params.transactionDataListLength;
+//       this.state = { 
+//          address: address,
+//          transactionDataList: transactionDataList,
+//          transactionDataListLength: transactionDataListLength
+//       }
+//    }
+  
+  
+//   componentDidMount() {
+//     // 'http://192.168.1.4:8080/addresses/0xF7b547f3E46EFfB3480EEE2c486AE760734B135c/transactions'
+//     //you need to change the ip to be the on that shows your localhost
+//     // in cmd you type ipconfig and you get IPv4 Address.
+//     const url = `${API_URL}/addresses/${this.props.route.params.address}/transactions`
+//     console.log(this.state);
+//     fetch(url
+//       ,
+//       {
+//         method: 'GET',
+//         headers: {
+//           'Accept': 'application/json;charset=utf-8',
+//           'Content-Type': 'application/json;charset=utf-8',
+//         },
+//       },
+//     )
+//     .then((response) => response.json())
+//     .then((responseJson) => {
+//       this.setState({ transactionDataList: responseJson });
+//       this.setState( {transactionDataListLength : responseJson.length})
+//     })
+//     .catch((error) => {
+//       console.log(error);
+//       this.setState({transactionDataListLength : 0})
+//     });
+//   }
+  
+
+//   render() {
+//     const tableData = [];
+
+//     const tableHead = ['From', 'To', 'Value'];
+
+//     const temp = this.state.transactionDataList.flatMap((e) => [e.from,e.to,e.value]);
+
+//     while(temp.length) {
+//       tableData.push(temp.splice(0,3));
+//     }
+    
+//     if (this.state.transactionDataListLength > 0) {
+//       return (
+//         <View>
+//           <Table>
+//             <Row data={tableHead} style={styles.head}  />
+//             <Rows data={tableData} style={styles.row}  />
+//           </Table>
+//         </View>
+//       );
+//      } else if (this.state.transactionDataListLength === 0) {
+//        return (
+//          <View>
+//           <Text>The Ethereum address that you searched has no transaction!</Text>
+//         </View>
+//       )
+//     } else {
+//       return null;
+//     }   
+//   }
+// }
 
 
  
