cc586ff776efded1856b4ce133ddf6f1b6954a87
Spyros <spyros.f4@gmail.com>
2020-12-21 05:20:14 +0200

    remove comments

--- reactNative/EthereumProject/components/Home/Home.js
+++ reactNative/EthereumProject/components/Home/Home.js
@@ -48,43 +48,6 @@ const Home = () => {
   );
 }
 
-// class Home extends Component {
-//   state = {
-//     address: '',
-//     transactionDataList : []
-//   };
-//   handleAddress = (text) => {
-//     this.setState({address: text});
-//   };
-//   search = (address) => {
-//     this.props.navigation.navigate('Transactions', {
-//       address: this.state.address, 
-//       transactionDataList : this.state.transactionDataList,
-//       transactionDataListLength: -1},
-//       );
-//   };
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <TextInput
-//           style={styles.input}
-//           underlineColorAndroid="transparent"
-//           placeholder="EthereumAddress"
-//           placeholderTextColor="#9a73ef"
-//           autoCapitalize="none"
-//           onChangeText={this.handleAddress}
-//         />
-
-//         <TouchableOpacity
-//           style={styles.searchButton}
-//           onPress={() => this.search(this.state.address)}>
-//           <Text style={styles.searchButtonText}> Search </Text>
-//         </TouchableOpacity>
-//       </View>
-//     );
-//   }
-// }
-
 const mapStateToProps = (state) => {
   const { transactions } = state
   return { transactions }
